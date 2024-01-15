5e85b91b760cc4a4298cc39d1e77489fb708ef6d
Juan Carlos Solórzano <jcarlossolc@gmail.com>
2024-01-15 10:21:39 -0600

    not really working rn but gotta port it to webpack and see if that works

--- popup.js
+++ popup.js
@@ -1,10 +1,19 @@
+import { getAnswer } from "./questionanswering";
+
+function displayAnswer(answer){
+  document.getElementById('contentContainer').innerText = "Respuesta recuperada: " + JSON.stringify(answer);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('askButton').addEventListener('click', function () {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        var currentTab = tabs[0];
-        chrome.tabs.sendMessage(currentTab.id, { action: 'getContent' }, function (response) {
-          document.getElementById('tabContent').innerText = response.content;
-        });
+  document.getElementById('askButton').addEventListener('click', async function () {
+    var userQuestion = document.getElementById('questionInput').value;
+    chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+      var currentTab = tabs[0];
+      chrome.tabs.sendMessage(currentTab.id, { action: 'getContent' }, async function (response) {
+        var pageContent = response.content;
+        var questionAnswer = await getAnswer(userQuestion, pageContent);
+        displayAnswer(questionAnswer);
       });
     });
   });
+});
--- questionanswering.js
+++ questionanswering.js
@@ -5,13 +5,25 @@ dotenv.config();
 const HF_TOKEN = process.env.HF_TOKEN
 const inference = new HfInference(HF_TOKEN);
 
-const answer = await inference.questionAnswering({
-    model: 'mrm8488/distill-bert-base-spanish-wwm-cased-finetuned-spa-squad2-es',
-    inputs: {
-      question: '¿Como me llamo?',
-      context: 'Soy Juan y tengo 22 años'
-    }
-  })
+async function getAnswer(question, context){
+  try{
+    const answer = await inference.questionAnswering({
+      model: 'mrm8488/distill-bert-base-spanish-wwm-cased-finetuned-spa-squad2-es',
+      inputs: {
+        question: question,
+        context: context
+      }
+    });
+    return answer;
+  }
+  catch (error){
+    console.error("Error durante inferencia: ", error);
+    return null;
+  }
+}
+
+export { getAnswer };
+
 
 console.log(answer);
 
